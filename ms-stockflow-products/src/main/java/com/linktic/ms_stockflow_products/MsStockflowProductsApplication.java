1e29a003d3caeaf74825a18da3b5e09c22e97130
NicolasSalazar <desarrollo@gracialab.co>
2025-11-11 18:15:30 -0500

    inicio del proyecto en java

---
+++ ms-stockflow-products/src/main/java/com/linktic/ms_stockflow_products/MsStockflowProductsApplication.java
@@ -0,0 +1,13 @@
+package com.linktic.ms_stockflow_products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsStockflowProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsStockflowProductsApplication.class, args);
+	}
+
+}
---
+++ ms-stockflow-products/src/test/java/com/linktic/ms_stockflow_products/MsStockflowProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linktic.ms_stockflow_products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsStockflowProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
